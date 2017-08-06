ca2220f7e6ed0eea7f55e27b3389fca7dece6d44
Tobias Herber <tobihrbr@gmail.com>
2017-08-06 12:08:10 +0200

    update to work with new files api

--- test/files.js
+++ test/files.js
@@ -1,31 +1,35 @@
 import test from 'ava';
-import f from '../lib/files';
+import f from '../lib/utils/files';
 import path from 'path';
 
 test('ignores files in gitignore', t => {
-	f(path.join(process.cwd(), 'helpers')).then((files) => {
-		for (var key in files) {
-			if (files[key].filename === 'ignore.js') {
-				t.fail();
-			}
-		}
+  f(path.join(process.cwd(), 'helpers'))
+    .then(files => {
+      for (var key in files) {
+        if (files[key].filename === 'ignore.js') {
+          t.fail();
+        }
+      }
 
-		t.pass();
-	}).catch(function (err) {
-		t.fail(err);
-	});
+      t.pass();
+    })
+    .catch(function(err) {
+      t.fail(err);
+    });
 });
 
 test('does not lint nodemodules', t => {
-	f(path.join(process.cwd(), 'helpers')).then((files) => {
-		for (var key in files) {
-			if (files[key].filename === path.join('node_modules', 'module.js')) {
-				t.fail();
-			}
-		}
+  f(path.join(process.cwd(), 'helpers'))
+    .then(files => {
+      for (var key in files) {
+        if (files[key].filename === path.join('node_modules', 'module.js')) {
+          t.fail();
+        }
+      }
 
-		t.pass();
-	}).catch(function (err) {
-		t.fail(err);
-	});
+      t.pass();
+    })
+    .catch(function(err) {
+      t.fail(err);
+    });
 });

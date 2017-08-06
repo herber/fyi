cc73b86cd27dc2d463c9a031a825f377daa95128
Tobias Herber <tobihrbr@gmail.com>
2017-08-06 12:08:27 +0200

    update logger

--- test/logger.js
+++ test/logger.js
@@ -1,24 +1,24 @@
 import test from 'ava';
-import l from '../lib/logger';
+import l from '../lib/utils/logger';
 
 test('logs', t => {
-  const err = [{
-		errors: [
-			{
-		    severity: 2,
-		    message: 'Strings must use singlequote.',
-		    line: 4,
-		    column: 15
-			}
-		],
-	  filename: 'test\\helpers\\error.js',
-	  errorCount: 1,
-	  warningCount: 0
-	}];
+  const err = [
+    {
+      errors: [
+        {
+          severity: 2,
+          message: 'Strings must use singlequote.',
+          line: 4,
+          column: 15
+        }
+      ],
+      filename: 'test\\helpers\\error.js',
+      errorCount: 1,
+      warningCount: 0
+    }
+  ];
 
-	if (l(err) !== null) {
-		t.pass();
-	};
+  if (l(err) !== null) {
+    t.pass();
+  }
 });
-
-// logger(errors)
